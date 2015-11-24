1b8c8b42cae48df4c521a8e9b35634de560254e4
johnkagga <johnkagga@gmail.com>
2015-11-24 23:31:18 +0300

    Created new data models for the weather data

---
+++ app/src/main/java/teamtreehouse/com/stormy/weather/Forecast.java
@@ -0,0 +1,11 @@
+package teamtreehouse.com.stormy.weather;
+
+/**
+ * Created by jokamjohn on 11/24/2015.
+ */
+public class Forecast {
+
+    private Current mCurrent;
+    private Hour[] mHour;
+    private Day[] mDay;
+}
