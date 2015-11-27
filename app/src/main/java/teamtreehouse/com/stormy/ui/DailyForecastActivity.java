5ac1da84dcae9da131739b259d0083fe5e3d2402
johnkagga <johnkagga@gmail.com>
2015-11-27 14:58:31 +0300

    Showing array data in a list

--- app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -2,6 +2,7 @@ package teamtreehouse.com.stormy.ui;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import teamtreehouse.com.stormy.R;
 
@@ -12,6 +13,24 @@ public class DailyForecastActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
+        //Showing the array data in a list
+        String[] daysOfTheWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                daysOfTheWeek
+        );
+
+        setListAdapter(adapter);
     }
 
 }
